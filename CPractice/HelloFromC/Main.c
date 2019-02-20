9a7a0e3a345221f0478f99f0a3ac4d839c3d7f27
TheAwesomeARay <andrewray4@gmail.com>
2019-02-19 21:48:06 -0600

    Excercise 1.8

--- CPractice/HelloFromC/Main.c
+++ CPractice/HelloFromC/Main.c
@@ -57,7 +57,17 @@ void NewLineCounter() {
 			++nl;
 
 	printf("%d\n", nl);
-	
+}
+
+void CountBlanksTabsAndNewLines() {
+	int c, nl;
+	nl = 0;
+
+	while ((c = getchar()) != EOF)
+		if (c == '\n' || c == '\t' || c == ' ')
+			++nl;
+
+	printf("%d\n", nl);
 }
 
 void main()
