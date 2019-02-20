10f040808e75d2abc49aa0527c0f8853933341c4
TheAwesomeARay <andrewray4@gmail.com>
2019-02-19 20:09:36 -0600

    Exercise 1.3

--- CPractice/HelloFromC/Main.c
+++ CPractice/HelloFromC/Main.c
@@ -8,10 +8,14 @@ void main()
 	lower = 0;
 	upper = 300;
 	step = 20;
+	printf("-------------------------------\n");
+	printf("Temperature Conversion Table\n");
+	printf("Fahr\tCelcius\n");
+	printf("-------------------------------\n");
 
 	fahr = lower;
 	while (fahr <= upper) {
-		celcius = 5.0 * (fahr - 32.0) / 9.0;
+		celcius = 5.0/9.0 * (fahr-32.0);
 		printf("%3.0f\t%6.1f\n", fahr, celcius);
 		fahr = fahr + step;
 	}
