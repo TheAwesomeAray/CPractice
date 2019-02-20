d1944df4081bf53bf7721a447343ba1f289e13b3
TheAwesomeARay <andrewray4@gmail.com>
2019-02-19 20:45:51 -0600

    Copy program

--- CPractice/HelloFromC/Main.c
+++ CPractice/HelloFromC/Main.c
@@ -4,8 +4,7 @@
 #define UPPER	300
 #define STEP	20
 
-void main()
-{
+void TempConverter() {
 	float fahr;
 
 	printf("-------------------------------\n");
@@ -17,4 +16,22 @@ void main()
 		printf("%3.0f\t%6.1f\n", fahr, 5.0 / 9.0 * (fahr - 32.0));
 
 	scanf_s("%s");
+	return;
 }
+
+void Copy() {
+	int c;
+
+	c = getchar();
+
+	while (c != EOF) {
+		putchar(c);
+		c = getchar();
+	}
+}
+
+void main()
+{
+	Copy();
+}
+
